cb1c0de2322df35a1a81086f3144a1a27b5252d7
darwin <darwinj94@hotmail.com>
2017-09-01 15:24:27 -0500

    cree el componente home con sus hijos noticias, tabla y slider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import{SharedModule} from './shared/shared.module';
+import{SharedModule} from './shared-module/shared.module';
+import { HomeComponent } from './components/home/home.component';
+import { TablaPosicionesComponent } from './components/home/tabla-posiciones/tabla-posiciones.component';
+import { NoticiasComponent } from './components/home/noticias/noticias.component';
+import { SliderComponent } from './components/home/slider/slider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TablaPosicionesComponent,
+    NoticiasComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
