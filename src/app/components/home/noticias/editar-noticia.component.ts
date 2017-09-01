476c88709bcca17b2ea6fc34a81f206a7db51909
OscarToaQuiza <oscartoaquiza388@gmail.com>
2017-09-01 18:35:54 -0500

    Tabla de posiciones

--- src/app/components/home/noticias/editar-noticia.component.ts
+++ src/app/components/home/noticias/editar-noticia.component.ts
@@ -1,18 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'editar-app-noticia',
   templateUrl: './agregar-noticia.component.html'
 })
 export class EditarNoticiaComponent implements OnInit {
-  public title:string;
 
-  constructor() { 
-    
-  }
+  constructor() {  }
 
-  ngOnInit() {
-      
-  }
+  ngOnInit() { }
 
 }
