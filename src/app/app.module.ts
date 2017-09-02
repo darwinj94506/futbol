24295cccfb4b7e2ff8b5e63347d802c3b7636918
darwin <darwinj94@hotmail.com>
2017-09-02 12:08:32 -0500

    se crearon las rutas con su componente respectivo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import{SharedModule} from './shared-module/shared.module';
+//Componentes 
 import { HomeComponent } from './components/home/home.component';
 import { TablaPosicionesComponent } from './components/home/tabla-posiciones/tabla-posiciones.component';
 import { VerNoticiasComponent } from './components/home/noticias/ver-noticias.component';
@@ -10,6 +10,15 @@ import { EditarNoticiaComponent } from './components/home/noticias/editar-notici
 import { AgregarNoticiaComponent } from './components/home/noticias/agregar-noticia.component';
 
 import { SliderComponent } from './components/home/slider/slider.component';
+import { EquiposComponent } from './components/equipos/equipos.component';
+import { CalendarioComponent } from './components/calendario/calendario.component';
+import { TemporadaComponent } from './components/temporada/temporada.component';
+
+//modulo creado
+import {SharedModule} from './shared-module/shared.module';
+
+//rutas
+import{RoutingModule} from './app.routing';
 
 @NgModule({
   declarations: [
@@ -19,11 +28,15 @@ import { SliderComponent } from './components/home/slider/slider.component';
     VerNoticiasComponent,
     SliderComponent,
     EditarNoticiaComponent,
-    AgregarNoticiaComponent
+    AgregarNoticiaComponent,
+    EquiposComponent,
+    CalendarioComponent,
+    TemporadaComponent
   ],
   imports: [
     BrowserModule,
-    SharedModule
+    SharedModule,
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routing.ts
@@ -0,0 +1,30 @@
+import{NgModule} from '@angular/core';
+import{RouterModule, Routes} from '@angular/router';
+import{HomeComponent} from './components/home/home.component';
+import{EquiposComponent} from './components/equipos/equipos.component';
+import{TemporadaComponent} from './components/temporada/temporada.component';
+
+// import{MainComponent} from './components/main/main.component';
+// import{ListComponent} from './components/list/list.component';
+// import{AddComponent} from './components/add/add.component';
+// import{EditComponent} from './components/edit/edit.component';
+
+// import{AdminGuard} from '../services/admin.guard';
+
+const adminRoutes:Routes=[
+  {path:'home',component:HomeComponent},
+  {path:'equipos',component:EquiposComponent},
+  {path:'temporada',component:TemporadaComponent},
+  {path:'',redirectTo:'home',pathMatch:'full'},
+  {path:'**',component:HomeComponent}
+];
+
+@NgModule({
+  imports:[
+    RouterModule.forRoot(adminRoutes)
+  ],
+  exports:[
+    RouterModule
+  ]
+})
+export class RoutingModule{}
