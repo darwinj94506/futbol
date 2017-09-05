e6dfc216ef85abfe6056b01813e3af933fd4333f
OscarToaQuiza <oscartoaquiza388@gmail.com>
2017-09-05 11:46:46 -0500

    estilos Globales de inputs

--- src/app/components/equipos/equipos.component.ts
+++ src/app/components/equipos/equipos.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./equipos.component.css']
 })
 export class EquiposComponent implements OnInit {
-
+  public formulario_equipo = true;
   constructor() { }
 
   ngOnInit() {
   }
-
+  ver_registro_equipo(event){
+    this.formulario_equipo = false;
+  }
+  cancelar_registro_equipo(event){
+    this.formulario_equipo = true;
+  }
 }
+
+
+
+
+ 
+ 
